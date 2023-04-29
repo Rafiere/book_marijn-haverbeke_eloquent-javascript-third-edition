d6b0bd8c431bd170444a949f6b72c1723d2706a2
Rafiere <rafapgbr@gmail.com>
2023-04-29 14:32:08 -0300

    feat: add chapter 12 code

---
+++ chapter12-project-a-programming-language/chapter12.js
@@ -0,0 +1,24 @@
+/* Parsing */
+
+console.log("\nParsing\n")
+
+/* Um "parser" é um programa que lê um pedaço de texto e produz uma estrutura de dados que
+* reflete a estrutura do programa contida no texto. Se o texto não forma uma estrutura
+* válida, o parser apontará o erro. */
+
+/* Evaluator */
+
+console.log("\nEvaluator\n")
+
+/* O evaluator recebe uma árvore síntatica e um objeto que associa nomes aos valores e realiza
+* a evaluação da expressão que representa e retorna o valor produzido. */
+
+/* Compilação */
+
+console.log("\nCompilação\n")
+
+/* Compilação é o processo de adicionar outro passo entre o parser e a execução do programa. Ele
+* transforma o programa em algo que possa ser evaluado de forma mais eficiente. */
+
+/* Na maioria dos casos, a compilação envolve converter o programa para código de máquina, que é o
+* formato cru que um processador pode executar. */
