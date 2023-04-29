caa1825db3a44d5991ed0caf01b02476af8a460d
Rafiere <rafapgbr@gmail.com>
2023-04-29 14:47:57 -0300

    feat: add chapter 13 code

---
+++ chapter13-javascript-and-the-browser/chapter13.js
@@ -0,0 +1,34 @@
+/* A Internet é uma forma de conectarmos máquinas ao redor do mundo. */
+
+/* Um protocolo de rede descreve o estilo de comunicação em uma rede. Há protocolos para
+* envio de email, compartilhamento de arquivos e etc. */
+
+/* O HTTP é um protocolo para obter recursos nomeados. Ele especifica que o lado que está
+* realizando a requisição deve começar com "GET /index.html HTTP/1.1", nomeando o recurso
+* e a versão do protocolo que ele está tentando acessar. */
+
+/* Uma conecção TCP funciona da seguinte forma: Um computador deve estar aguardando ou ouvindo
+* outros computadores e começam a falar com ele. Para poder escutar diferentes tipos de comunicação
+* ao mesmo tempo, em uma única máquina, cada ouvinte tem uma porta associada a ele. */
+
+/* A maioria dos protocolos especificam qual porta deve ser usada por padrão. Quando enviamos
+* um email com o protocolo SMTP, por exemplo, a máquina está esperando ser ouvida na porta
+* 25. */
+
+/* A World Wide Web é um conjunto de protocolos e formatos que nos permitem visitar páginas
+* em um browser. O "Web" diz que as páginas podem facilmente se conectarem umas as outras. */
+
+/* Para nos conectrmos a internet, precisamos apenas conectar uma máquina na Internet que
+* está escutando na porta 80. */
+
+/* Cada documento na Web possui uma URL, que é dividida entre "protocolo", "servidor" e
+* "caminho", como: "http://eloquentjavascript.net/13_browser.html". */
+
+/* HTML */
+
+console.log("\nHTML\n")
+
+/* O HTML é o formato de documento que é utilizado para páginas web. */
+
+/* Dentro da tag "<script>" do HTML, podemos inserir códigos em JavaScript. */
+
