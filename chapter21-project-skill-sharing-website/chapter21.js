72aea001634bce75b38605f142873bb526d460f0
Rafiere <rafapgbr@gmail.com>
2023-04-30 08:55:20 -0300

    feat: add chapter 21 code

---
+++ chapter21-project-skill-sharing-website/chapter21.js
@@ -0,0 +1,6 @@
+/* O conceito de "WebSockets" permite que conexões sejam abertas para troca arbitrária
+* de dados. */
+
+/* Nesse capítulo, foi criado um site para compartilhar informações entre
+* desenvolvedores apenas usando o JS. Não foi utilizado nenhuma ferramenta que
+* abstrai essa criação, como o ExpressJS, por exemplo. */
